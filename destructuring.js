285b3fdc6aa90a43921554610ae5d27496868652
Natete-Verite <nateteverite@gmail.com>
2022-07-19 13:27:38 +0300

    js revision

---
+++ destructuring.js
@@ -0,0 +1,58 @@
+// let a = [2,4,7]
+// let [one,four,seven, ...others] = [2,4,7,1,5,8];
+// console.log({one});
+// console.log({four});
+// console.log({seven});
+
+// let fruits ={
+//     name: "Mango",
+//     color: "Green",
+//     size: "small"
+// };
+// let{name,...rest}=fruits;
+// console.log(name);
+// console.log(rest);
+
+// let mother = "Beautiful";
+// console.log(mother);
+
+// let promise = new Promise((resolve,reject)=>{
+    // if (false) {
+    // resolve("Promise fulfilled")
+    // }
+    // else{
+    //     reject("Promise not fulfilled")
+    // } 
+//     if (true) {
+//         resolve("Promise fulfilled")
+//         }
+//     else{
+//             reject("Promise not fulfilled")
+//         }
+// })
+// .then(() => console.log("Successful"))
+// .catch(() => console.log("Promise failed"))
+// .finally(() => console.log("Promises are fun"))
+// console.log(promise);
+
+//     setTimeout(()=>resolve("Promise fullfiled"), 10000)
+
+// })
+// async function student(){
+//     let person = await promise;
+//     console.log(person);
+//     console.log("Await is only used in async functions");
+// }
+// student()
+
+// let num = 34;
+// console.log(num + 2);
+
+// let c = "Hello";
+// console.log(c);
+
+console.log("Plant Kunde");
+setTimeout(function(){
+console.log("Water kunde");
+}, 6000)
+console.log("Add fertilizers");
